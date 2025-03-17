9b48a80ee10f4fd28d2dfd96a315269fbf5c6834
KindaSnoowy <gabrielcecote1@gmail.com>
2025-03-16 23:27:48 -0300

    teste

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from extensions import app, db, socketio
 from routes import *  # noqa: F403
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # teste teste mudança linha 4
     with app.app_context():
         db.create_all()
     socketio.run(app, debug=True)
