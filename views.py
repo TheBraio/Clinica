dd18698a4885247b2d82e3e5c59d4644dd7a1e71
TheBraio <bryancarvalho20@gmail.com>
2025-03-17 23:57:01 -0300

    Adicionando autenticação

--- views.py
+++ views.py
@@ -1,25 +1,41 @@
 from extensions import app
 from flask import render_template
 from models import Pacientes
+from views_recursos import *
 
-@app.route("/", methods=["GET"])
+
+
+
+@app.route('/login', methods=["GET"])
+def login():
+    return render_template('login.html', links = links[session['nome']])
+
+
+# Rotas do admin
+@app.route('/admin-funcionarios')
+@is_admin
+def admin_funcionarios():
+    return render_template('admin_funcionarios.html', links = links[session['nome']])
+# Rotas do admin
+
+# Rotas do atendente
+@app.route("/atendente-pacientes", methods=["GET"])
 def ler():
     pacientes = Pacientes.query.all()
-    return render_template("home.html", pacientes=pacientes)
+    #return render_template("home.html", pacientes=pacientes,)
+    return render_template('home.html')
 
-
-@app.route("/cadastrar")
+@app.route("/atendente-cadastrar")
 def cadastrar():
     return render_template("cadastrarPaciente.html")
+# Rotas do atendente
 
-
-@app.route("/doutor")
+# Rotas do doutor 
+@app.route("/doutor-mainpage")
 def doutor():
     return render_template("doutor.html")
+# Rotas do doutor
 
-@app.route("/teste", methods=["GET"])
-def teste():
-    return render_template("teste.html")
 
 @app.route("/chamada")
 def chamada():
