bc67b1a467fd41fa1b413ee4c800e0eb1ad7d706
KindaSnoowy <gabrielcecote1@gmail.com>
2025-03-18 14:39:39 -0300

    consertando

--- routes.py
+++ routes.py
@@ -1,43 +1,21 @@
 from flask import request, redirect
 from extensions import app, db
 from models import Pacientes
-
-@app.route("/", methods=["GET"])
-def ler():
-    pacientes = Pacientes.query.all()
-    return render_template("home.html", pacientes=pacientes)
-
-
-####
-#### Pacientes CRUD
-####
-
-
-@app.route("/pacientes")
-def paciente_view():
-    pacientes = Pacientes.query.all()
-    return render_template("paciente_view.html", pacientes=pacientes)
-
-
-@app.route("/pacientes/cadastrar")
-def cadastrar():
-    return render_template("cadastrarPaciente.html")
-
-
-@app.route("/pacientes/cadastrar", methods=["POST"])
 from views import *
 from websocket import *
 
-@app.route("/login", methods=['POST'])
+
+@app.route("/login", methods=["POST"])
 def loginSend():
-    nome = request.form['nome']
-    senha = request.form['senha']
-    
-    if nome == 'admin':
-        if senha == '123':    
+    nome = request.form["nome"]
+    senha = request.form["senha"]
+
+    if nome == "admin":
+        if senha == "123":
             session["nome"] = nome
             return redirect("/admin-funcionarios")
     return redirect("/")
+
 
 @app.route("/cadastrar/enviar", methods=["POST"])
 def cadastroEnviar():
@@ -61,18 +39,3 @@ def deletar(id):
         db.session.commit()
 
     return redirect("/")
-
-@app.route("/doutor")
-def doutor():
-    return render_template("doutor.html")
-
-
-@app.route("/teste", methods=["GET"])
-def teste():
-    return render_template("teste.html")
-
-
-# Fila de pacientes
-
-queue = Queue()
-paciente_atual = None
--- views.py
+++ views.py
@@ -4,36 +4,46 @@ from models import Pacientes
 from views_recursos import *
 
 
-
-
-@app.route('/login', methods=["GET"])
+@app.route("/login", methods=["GET"])
 def login():
-    return render_template('login.html', links = links[session['nome']])
+    return render_template("login.html", links=links[session["nome"]])
 
 
 # Rotas do admin
-@app.route('/admin-funcionarios')
+@app.route("/admin-funcionarios")
 @is_admin
 def admin_funcionarios():
-    return render_template('admin_funcionarios.html', links = links[session['nome']])
+    return render_template("admin_funcionarios.html", links=links[session["nome"]])
+
+
 # Rotas do admin
+
 
 # Rotas do atendente
 @app.route("/atendente-pacientes", methods=["GET"])
 def ler():
     pacientes = Pacientes.query.all()
-    
-    return render_template("home.html", pacientes=pacientes,)
+
+    return render_template(
+        "home.html",
+        pacientes=pacientes,
+    )
+
 
 @app.route("/atendente-cadastrar")
 def cadastrar():
     return render_template("cadastrarPaciente.html")
+
+
 # Rotas do atendente
 
-# Rotas do doutor 
+
+# Rotas do doutor
 @app.route("/doutor-mainpage")
 def doutor():
     return render_template("doutor.html")
+
+
 # Rotas do doutor
 
 
